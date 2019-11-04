635bbeba538ed9d10986decc0e3a6a65a03b6ad4
owinolawrence <owinolawrence21@gmail.com>
2019-11-04 22:37:52 +0300

    import class profileservice

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService} from '../profile.service'
 
 @Component({
   selector: 'app-profile',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  profile:any;
+  
+  
+  constructor(private profileServices:ProfileService) {
+    this.profileServices.getProfileInfo().subscribe(profile=> {
+      console.log(profile);
+      this.profile = profile;
+    });
+   }
 
   ngOnInit() {
   }

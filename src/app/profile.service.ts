da7a7223c952b2d026cf5734d651806b4b7a39bd
owinolawrence <owinolawrence21@gmail.com>
2019-11-04 23:39:52 +0300

    add method ProfileService.getProfileRepos

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -17,4 +17,8 @@ export class ProfileService {
    getProfileInfo(){
      return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
    }
+   getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+   
+   }
 }

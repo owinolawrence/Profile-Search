80177662e9931e86b4271bb96152963515b20e4b
owinolawrence <owinolawrence21@gmail.com>
2019-11-04 22:35:29 +0300

    add properties to profileservice class

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient ,HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username :string;
+  private clientid = '3dfeefac56a73af0baa0';
+  private clientsecret= '9a4256cc5070d33c398836e792a1ee13552a856a';
+
+  constructor( private http:HttpClient) {
+    console.log("service is now ready");
+    this.username = "owinolawrence";
+   }
+   getProfileInfo(){
+     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+   }
 }

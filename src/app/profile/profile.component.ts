ee5aaa8299ad6b2259a8af97b28118603b834535
owinolawrence <owinolawrence21@gmail.com>
2019-11-05 06:53:15 +0300

    create properties to method find profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileComponent implements OnInit {
   profile:any;
   repos:any;
+  username :string;
   
   
   constructor(private profileService:ProfileService) {
@@ -20,10 +21,20 @@ export class ProfileComponent implements OnInit {
     this.profileService.getProfileRepos().subscribe(repos => {
       console.log(repos);
       this.repos =repos;
+    })
+  }
+    
+    findProfile(){
+      this.profileService.updateProfile(this.username);this.profileService.getProfileInfo().subscribe(profile=> {
+        console.log(profile);
+        this.profile = profile;
+      });
+      this.profileService.getProfileRepos().subscribe(repos => {
+        console.log(repos);
+        this.repos =repos;
+      })
     }
-
-    )
-   }
+   
 
   ngOnInit() {
   }

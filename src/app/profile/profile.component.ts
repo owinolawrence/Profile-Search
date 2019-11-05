116da5f8eece8fd1d7641f6d4dbcfb6ff43cb8bb
owinolawrence <owinolawrence21@gmail.com>
2019-11-05 09:26:13 +0300

    fix identation and spacing error

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,33 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  profile:any;
-  repos:any;
-  username :string;
-  
-  
-  constructor(private profileService:ProfileService) {
-    this.profileService.getProfileInfo().subscribe(profile=> {
+  profile: any;
+  repos: any;
+  username: string;
+
+
+  constructor(private profileService: ProfileService) {
+    this.profileService.getProfileInfo().subscribe(profile => {
       console.log(profile);
       this.profile = profile;
     });
     this.profileService.getProfileRepos().subscribe(repos => {
       console.log(repos);
-      this.repos =repos;
+      this.repos = repos;
     })
   }
-    
-    findProfile(){
-      this.profileService.updateProfile(this.username);this.profileService.getProfileInfo().subscribe(profile=> {
-        console.log(profile);
-        this.profile = profile;
-      });
-      this.profileService.getProfileRepos().subscribe(repos => {
-        console.log(repos);
-        this.repos =repos;
-      })
-    }
-   
+
+  findProfile() {
+    this.profileService.updateProfile(this.username); this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
+  }
+
 
   ngOnInit() {
   }

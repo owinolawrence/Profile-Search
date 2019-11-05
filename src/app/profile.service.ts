813c10e03d1a3dacacb1b33e48ee39a060578e34
owinolawrence <owinolawrence21@gmail.com>
2019-11-05 09:26:04 +0300

    fix identation and spacing error

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient ,HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  private username :string;
+  private username: string;
   private clientid = '3dfeefac56a73af0baa0';
-  private clientsecret= '9a4256cc5070d33c398836e792a1ee13552a856a';
+  private clientsecret = '9a4256cc5070d33c398836e792a1ee13552a856a';
 
-  constructor( private http:HttpClient) {
+  constructor(private http: HttpClient) {
     console.log("service is now ready");
     this.username = "owinolawrence";
-   }
-   getProfileInfo(){
-     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
-   }
-   getProfileRepos(){
+  }
+  getProfileInfo() {
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+  }
+  getProfileRepos() {
     return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
-   
-   }
-   updateProfile(username :string){
-     this.username = username;
-   }
+
+  }
+  updateProfile(username: string) {
+    this.username = username;
+  }
 }
